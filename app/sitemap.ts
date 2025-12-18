0cd6158fa9d3e7df67938752792f162c1f1ef8b2
Juljan Zenelaj <juljanzenelaj@gmail.com>
2025-12-18 13:17:45 +0100

    Fix sitemap & robots MetadataRoute import

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,4 +1,4 @@
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
@@ -8,3 +8,4 @@ export default function sitemap(): MetadataRoute.Sitemap {
     },
   ];
 }
+
